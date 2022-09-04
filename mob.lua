68f091bc9d910c4f473cc4d9949fd1627e04494a
Wesley Acheson <wesley.acheson@easypaymentgateway.com>
2022-09-04 18:43:01 +0100

    Work in progress

--- mob.lua
+++ mob.lua
@@ -28,38 +28,33 @@ function create_mob(s,x,y,w,h)
 		coffy=0,
 		flash=0
 	}
+	setmetatable(mob,{__index=_ENV})
 	return mob
 end
 
-function def_move(self)
-		self.x+=self.dx
-		self.y+=self.dy
+function def_move(_ENV)
+		x+=dx
+		y+=dy
 end
 
-function def_draw(self)
-		if self.flash>0 then
+function def_draw(_ENV)
+		if flash>0 then
 			pal(lightenpal[1])
 		end
-		if self.flash>2 then
+		if flash>2 then
 			pal(lightenpal[2])
 		end
-		spr(
-			self.s,
-			self.x-self.pw/2,
-			self.y-self.ph/2,
-			self.w,
-			self.h)
+		spr(s,x-pw/2,y-ph/2,w,h)
 			pal()
-		draw_collision(self)
+		draw_collision(_ENV)
 end
 
-function def_col(self)
-		local s=self
-		local col_width=s.colw or s.pw-1
-		local col_height=s.colh or s.ph-1
-		local coffx=s.coffx or 0
-		local coffy=s.coffy or 0
-		local l,t=s.x-col_width/2,s.y+coffy-col_height/2
+function def_col(_ENV)
+		local col_width=colw or pw-1
+		local col_height=colh or ph-1
+		local coffx=coffx or 0
+		local coffy=coffy or 0
+		local l,t=x-col_width/2,y+coffy-col_height/2
 		return {
 			l,
 			t,
@@ -67,19 +62,19 @@ function def_col(self)
 			t+col_height}
 end
 
-function def_hurt(self, pow)
-				self.flash=5
-				self.hp-=pow
-				if self.hp <= 0 then
-					self:die()
+function def_hurt(_ENV, pow)
+				flash=5
+				hp-=pow
+				if hp <= 0 then
+					die(_ENV)
 				else 
 					sfx(61)
 				end 
 end
 
-function def_die(self)
-	mob_to_ppart(self)
-	remove(self)
-	score+=self.score*score_mult>>>16
+function def_die(_ENV)
+	mob_to_ppart(_ENV)
+	remove(_ENV)
+	score+=score*score_mult>>>16
 	sfx(60)
 end
